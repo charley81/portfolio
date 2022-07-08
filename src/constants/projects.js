9455a44413d46794445791797494d3d911bc70f4
ChrisKnowsDev <chrisharley81@gmail.com>
2022-07-08 08:01:51 -0400

    removed ux projects

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,36 +1,18 @@
 const projects = [
   {
     id: 1,
-    title: 'checkout express',
-    subtitle: 'Bookstore E-Commerce | UX/UI Design',
-    link: 'https://google.com',
-  },
-  {
-    id: 2,
-    title: 'cap house music',
-    subtitle: 'Music Festival | UX/UI Design',
-    link: 'https://google.com',
-  },
-  {
-    id: 3,
-    title: 're-imagine search',
-    subtitle: 'Search Engine | UX/UI Design',
-    link: 'https://google.com',
-  },
-  {
-    id: 4,
     title: 'enter the stu',
     subtitle: 'Recoding Studio | Visual Design & Development',
     link: 'https://soundbossstudios.netlify.app/',
   },
   {
-    id: 5,
+    id: 2,
     title: 'grab a brew',
     subtitle: 'Beer Brewery | Visual Design & Development',
     link: 'https://luckyguybrewing.netlify.app/',
   },
   {
-    id: 6,
+    id: 3,
     title: 'digital jamming',
     subtitle: 'Digial Agency | Visual Design & Development',
     link: 'https://thegreatdevco.netlify.app/',
